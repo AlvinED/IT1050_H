cc5f42b76c3f9fa1c9024904c79a3a407fc7c0b4
AlvinED <Alvin.Knows@Hotmail.com>
2015-01-31 13:19:02 -0500

    Lab One submission
    Lab 1_1 needs more work

--- IT 1050 Lab 1 aew/IT 1050 Lab 1/Program_a.cs
+++ IT 1050 Lab 1 aew/IT 1050 Lab 1/Program_a.cs
@@ -8,18 +8,15 @@ namespace IT_1050_Lab_1
             string firstName = "Alvin";
             char middleInitial = 'E';
             string lastName = "williams";
-            string fullName = firstName + middleInitial + lastName;
+            string fullName = firstName +" " + middleInitial +". " + lastName;
             int age = 61;
-            bool isCitizen = true ;
+            bool isCitizen = true;
             
             int heightFeet = 5;
             double heightInches = 9.5;
             double totalHeightCM = ((heightFeet *12) + heightInches) * 2.54;
             bool canVote = age >= 21 && isCitizen;
-
-
-
-
+            
         }
     }
 }
--- Lab 1_1/Lab 1_1/Program.cs
+++ Lab 1_1/Lab 1_1/Program.cs
@@ -5,21 +5,35 @@ namespace Lab_1_1
     {
         static void Main(string[] args)
         {
+            //Input lines
             Console.WriteLine("Please enter your first name");
             string firstName = Console.ReadLine();
             Console.WriteLine("Please enter your middle initial");
-            string middleInitial = Console.ReadLine();
+            Char middleInitial = Console.ReadKey().KeyChar;
+            Console.WriteLine();
             Console.WriteLine("Please enter your last name");
             string lastName = Console.ReadLine();
-            string fullName = firstName + middleInitial + lastName;
-            int age = 61;
-            bool isCitizen = true;
-
-            int heightFeet = 5;
-            double heightInches = 9.5;
-            Console.WriteLine(firstName +" "+ middleInitial +" " + lastName);
+            Console.WriteLine("Voting Eligibility"); Console.WriteLine("please enter the following informtion");
+            Console.WriteLine("Age");
+            int age = Console.Read();
+            Console.WriteLine("Are your a citizen, please enter y or n");
+            char CitizenYayNay = Console.ReadKey(Equals('y', 'n')).KeyChar;
+            bool canVote = CitizenYayNay == 'y'; Console.WriteLine(" ");
+            Console.WriteLine("What is your hieght in feet?");
+            int heightFeet = (Console.Read());
+            Console.ReadKey();
+            Console.WriteLine("What is your hieght in inches?");
+            double heightInches = Console.Read();
+            Console.ReadKey();
             double totalHeightCM = ((heightFeet * 12) + heightInches) * 2.54;
-            bool canVote = age >= 21 && isCitizen;
+            //Format Output lines
+            string fullName = firstName +" "+ middleInitial +". "+ lastName;
+            // Output
+            Console.WriteLine();
+            Console.WriteLine(fullName); Console.WriteLine();
+            Console.WriteLine(totalHeightCM);
+            /// this need more work can't spend any more time will resubmit aew
+           
         }
     }
 }
