85c43f8a7e460d3d7a1f5213c753eb8b21d2c34e
AlvinED <Alvin.Knows@Hotmail.com>
2015-02-03 17:51:41 -0500

    Update Lab 2
    Add lab 2 and cleanup

---
+++ Lab2_aew/Lab2/LinkedList.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace Lab2
+{
+    class LinkedList
+    {
+        public int size;
+        public Node root;
+        public Node Last;
+
+               
+    }
+}
---
+++ Lab2_aew/Lab2/Node.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+namespace Lab2
+{
+    class Node
+    {
+        public string Value;
+        public Node next;
+
+
+
+
+    }
+}
