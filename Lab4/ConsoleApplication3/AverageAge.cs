7efa00231cdaa2bb66ec5dd5013beb4452942e00
AlvinED <alvin.knows@hotmail.com>
2015-03-24 15:55:31 -0400

    Lab 4
    Just starting

---
+++ Lab4/ConsoleApplication3/AverageAge.cs
@@ -0,0 +1,19 @@
+﻿namespace Lab4
+{
+    public class AverageAge
+    {
+        private double _sumOfAllAges;
+        private int _totalNumberOfPeople;
+
+        public AddPerson()
+        {
+            //C sharp average
+            //Double [] array1 = {1, 2, 3, 5, 0};
+            //Double average1 = array1.Average ();
+            //Console.WriteLine (average1);
+
+        }
+            
+
+    }
+}
---
+++ Lab4/ConsoleApplication3/Person.cs
@@ -0,0 +1,33 @@
+﻿using System;
+
+namespace Lab4
+{
+    public class Person
+    {
+        public int age;
+        public string firstName;
+        public string lastName;
+        public Person Spouse;
+        public int sAge;
+
+        public string Create()
+        {
+            string firstName = Question.AskForString("What is your frist name?");
+            string lastName = Question.AskForString("What is your last name?");
+            int age = Question.AskForInteger("What your current age?");
+            bool y = Question.AskForBoolean("Are you married?");
+            if (y)
+            {
+                string Spouse = Question.AskForString("What is your Spouse frist name?");
+                int sAge = Question.AskForInteger("What your Spouse age?");
+                return firstName + lastName + age + Spouse + sAge;
+            }
+            else
+            {
+                return  firstName + lastName + age;
+            }
+        }
+
+        
+    }
+}
