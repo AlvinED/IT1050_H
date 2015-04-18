24918d8ced0178948921b554e338772b1844e7a0
AlvinED <Alvin.Knows@Hotmail.com>
2015-04-18 10:56:11 -0400

    Lab 5 update
    It's only Saturday. The day is still jeune.

--- Lab 5 NewCourse/Lab 5/Course.cs
+++ Lab 5 NewCourse/Lab 5/Course.cs
@@ -6,7 +6,7 @@ namespace Lab_5
     {
         public string cName;
         public int cIDnumber;
-        public int clasSize;
+        
         public Student[] eNrolled;
 
         public void createCourse()
@@ -16,10 +16,9 @@ namespace Lab_5
             myClass.cName = Console.ReadLine();
             Console.WriteLine("Please enter course number");
             myClass.cIDnumber = int.Parse(Console.ReadLine());
-            Console.WriteLine("Please enter Max number of students");
-            myClass.clasSize = int.Parse(Console.ReadLine());
-            myClass.eNrolled = new Student [myClass.clasSize];
-            for (Student eNrolled X = 0 ; X < eNrolled.Length; Student eNrolled++)
+            
+            myClass.eNrolled = new Student [clasSize];
+            for ( eNrolled X = 0 ; X < eNrolled.Length;  eNrolled++)
             {
                 Student.CreateStudent();
             }
---
+++ Lab 5 NewCourse/Lab 5/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+namespace Lab_5
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("To create a new course. Enter Y if you done enter - N");
+            {
+                string courseNew = Console.ReadLine();
+            }
+            
+            Console.WriteLine("Please enter Max number of students");
+            int clasSize = int.Parse(Console.ReadLine());
+            Course.createCourse();
+
+            
+        }
+    }
+}
